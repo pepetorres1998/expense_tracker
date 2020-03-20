2fe69693642f3d8657ef4852dcfadbc0566bfd9b
Jose Antonio Torres Garibay <jtorres@syscap.com.mx>
2020-03-19 19:23:46 -0600

    Add placeholder response

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -12,13 +12,16 @@ module ExpenseTracker
 
     it 'records submitted expenses' do
       coffee = {
-        'payee': 'Starbucks',
-        'amount': 5.75,
-        'date': '2017-06-10'
+        'payee' => 'Starbucks',
+        'amount' => 5.75,
+        'date' => '2017-06-10'
       }
 
       post '/expenses', JSON.generate(coffee)
       expect(last_response.status).to eq(200)
+
+      parsed = JSON.parse last_response.body
+      expect(parsed).to include('expense_id' => a_kind_of(Integer))
     end
   end
 end
